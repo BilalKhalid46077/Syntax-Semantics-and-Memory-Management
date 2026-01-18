972502e86bf67aafa1b413008001fbef7dad718d
Bilal Khalid <bkhalid46077@ucumberlands.edu>
2026-01-18 20:28:58 +0500

    JavaScript Semantics Code

---
+++ Part1_Syntax_and_Semantics/Section2_Semantics/javascript_semantics.js
@@ -0,0 +1,11 @@
+function showTypeBehavior() {
+    let value = 10;
+    console.log("Value:", value);
+    console.log("Type:", typeof value);
+
+    value = "Ten";
+    console.log("Value:", value);
+    console.log("Type:", typeof value);
+}
+
+showTypeBehavior();
