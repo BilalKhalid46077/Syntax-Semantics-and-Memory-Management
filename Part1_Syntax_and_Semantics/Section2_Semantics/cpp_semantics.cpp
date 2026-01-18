404990e84bf2234ee92ea5202216c5ceacd7092a
Bilal Khalid <bkhalid46077@ucumberlands.edu>
2026-01-18 20:30:04 +0500

    C++ Semantics Code

---
+++ Part1_Syntax_and_Semantics/Section2_Semantics/cpp_semantics.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int value = 10;
+    cout << "Value: " << value << endl;
+
+    // value = "Ten";  // This line causes a compilation error
+    return 0;
+}
